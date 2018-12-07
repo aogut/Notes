0d35fc364688cb13fc5ecfb84830fe04de818e5f
aogut <andrew.ao@gmail.com>
2018-12-07 14:56:43 -0800

    Create DB for Node.js

---
+++ DB for Node.js
@@ -0,0 +1,13 @@
+# Docker
+```
+docker pull mongo                           
+
+// -p mapping port local:docker
+docker run --name mongodb -p 37017:27017 -d mongo   -d background
+
+docker ps
+
+docker start mongodb
+```
+
+* Robo 3T - Gui
