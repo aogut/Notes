83635c722c3f60c3e8743e71004ce2691b5d30ac
aogut <andrew.ao@gmail.com>
2018-12-10 14:33:54 -0800

    Update DB for Node.js

--- DB for Node.js
+++ DB for Node.js
@@ -8,6 +8,9 @@ docker run --name mongodb -p 37017:27017 -d mongo   -d background
 docker ps
 
 docker start mongodb
+
+docker pull mysql
+docker run --name mysql -p 3406:3306 -e MYSQL_ROOT_PASSWORD=mypassword -d mysql
 ```
 
 * Robo 3T - Gui
@@ -42,3 +45,31 @@ function insert(collection, data) {
   return Promise.all(promisedInserts);
 }
 ```
+
+### MySQL
+* mysql workbench
+* npm mysql2
+
+```js
+function insert(connection, data, cb) {
+  const values = [];
+  const sql = 'INSERT INTO coinvalues (valuedate, coinvalue) VALUES ?';
+  Object.keys(data).forEach((key) => {
+    values.push([key, data[key]]);
+  });
+  connection.query(sql, [values], cb);
+}
+
+const connection = mysql.createConnection({
+  host:, port, 
+})
+
+connection.connect((err) => {
+  fetchFromAPI((err, data) => {
+    insert(connection, [...], () => {})
+  });
+})
+```
+
+### Sequelize - OR Mapper
+
